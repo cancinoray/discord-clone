4e12b300575cb163436fc60e18b65b5e3a1459a8
Ray Cancino <cancinoray@gmail.com>
2023-12-19 01:19:42 +0800

    style:dark-light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex p-2 space-x-2">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 };
